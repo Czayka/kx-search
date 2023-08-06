39154d7b3b671fc1ee3f59f0e23be09e360d053b
Administrator <1606007912@qq.com>
2023-08-06 23:09:20 +0800

    update

--- src/main/java/com/example/config/MybatisPlusConfig.java
+++ src/main/java/com/example/config/MybatisPlusConfig.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
