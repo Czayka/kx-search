cc8a60ef725f2d8b809f466cdf2c943fc96c4262
Administrator <1606007912@qq.com>
2023-08-06 23:02:59 +0800

    update

--- src/main/java/com/example/KxSearchApplication.java
+++ src/main/java/com/example/KxSearchApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.example.mapper")
 public class KxSearchApplication {
 
     public static void main(String[] args) {
